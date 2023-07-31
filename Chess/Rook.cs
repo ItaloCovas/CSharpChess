83700faf561dc3c24d574c2976c9eee553f6cad7
Italo Covas <italocovas@gmail.com>
2023-07-31 15:09:29 -0300

    chore: rook possible movements

--- Chess/Rook.cs
+++ Chess/Rook.cs
@@ -10,6 +10,74 @@ namespace CSharpChess.Chess
 
         }
 
+        private bool CanMove(Position position)
+        {
+            Piece piece = Board.GetPiece(position);
+
+            return piece == null || piece.Color != Color;
+        }
+
+        public override bool[,] PossibleMovements()
+        {
+            bool[,] movements = new bool[Board.Rows, Board.Columns];
+
+            Position position = new Position(0, 0);
+
+            //Above
+            position.SetValues(Position.Row - 1, Position.Column);
+            while (Board.IsPositionValid(position) && CanMove(position))
+            {
+                movements[position.Row, position.Column] = true;
+                if (Board.GetPiece(position) != null && Board.GetPiece(position).Color != Color)
+                {
+                    break;
+                }
+                position.Row = position.Row - 1;
+            }
+
+            //Below
+            position.SetValues(Position.Row + 1, Position.Column);
+            while (Board.IsPositionValid(position) && CanMove(position))
+            {
+                movements[position.Row, position.Column] = true;
+                if (Board.GetPiece(position) != null && Board.GetPiece(position).Color != Color)
+                {
+                    break;
+                }
+                position.Row = position.Row + 1;
+            }
+
+            //Right
+            position.SetValues(Position.Row, Position.Column + 1);
+            while (Board.IsPositionValid(position) && CanMove(position))
+            {
+                movements[position.Row, position.Column] = true;
+                if (Board.GetPiece(position) != null && Board.GetPiece(position).Color != Color)
+                {
+                    break;
+                }
+                position.Column = position.Column + 1;
+            }
+
+            //Right
+            position.SetValues(Position.Row, Position.Column - 1);
+            while (Board.IsPositionValid(position) && CanMove(position))
+            {
+                movements[position.Row, position.Column] = true;
+                if (Board.GetPiece(position) != null && Board.GetPiece(position).Color != Color)
+                {
+                    break;
+                }
+                position.Column = position.Column - 1;
+            }
+
+
+            return movements;
+
+        }
+
+
+
         public override string ToString()
         {
             return "R";
